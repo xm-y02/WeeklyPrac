55ff216a21b6e77ece99ce6eb9dd128f9cd7c381
xm-y02 <xinmeng.yang@my.jcu.edu.au>
2022-12-18 18:26:35 +0800

    languages

---
+++ prac_06/languages.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practical
+Languages
+Author: Xinmeng Yang
+"""
+
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
+
